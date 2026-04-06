d07a9ae15441cabcd634539c4a09c8a81d3779ca
Jacob Hillock <jacob@hillock.cc>
2026-04-06 15:00:25 -0400

    chore: migrate to vite plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite-plus'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  fmt: {},
+  lint: {"options":{"typeAware":true,"typeCheck":true}},
   plugins: [react()],
   ...(process.env.INCLUDE_BASE ? { base: './' } : {}),
   server:{
